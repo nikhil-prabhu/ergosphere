72eb1b1508eb8febe3c81bfdec88eca131f99694
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2026-05-25 19:24:30 +0530

    feat(config): add sync options

--- src/config.rs
+++ src/config.rs
@@ -27,12 +27,15 @@ use std::path::Path;
 use config::{Config, ConfigError, Environment, File};
 use serde::{Deserialize, Serialize};
 
+use crate::api::types::GravityImportOptions;
+
 /// Strongly-typed structural map of all runtime parameters of Ergosphere.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AppConfig {
     pub daemon: DaemonSettings,
     pub primary: NodeSettings,
     pub replicas: Vec<NodeSettings>,
+    pub sync: SyncSettings,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -53,6 +56,28 @@ pub struct NodeSettings {
     pub password: String,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SyncSettings {
+    /// Synchronize everything (i.e. enable all Teleporter import options).
+    /// If `true` overrides all other sync options.
+    #[serde(default = "default_true")]
+    pub full_sync: bool,
+    #[serde(default = "default_false")]
+    pub config: bool,
+    #[serde(default = "default_false")]
+    pub dhcp_leases: bool,
+    #[serde(default)]
+    pub gravity: GravityImportOptions,
+}
+
+fn default_true() -> bool {
+    true
+}
+
+fn default_false() -> bool {
+    false
+}
+
 impl AppConfig {
     /// Layer and load configuration from disk files and environment overrides.
     pub fn load() -> Result<Self, ConfigError> {
@@ -62,6 +87,16 @@ impl AppConfig {
         let mut builder = Config::builder()
             .set_default("daemon.debounce_seconds", 3)?
             .set_default("daemon.watch_directory", String::from(PIHOLE_CONFIG_DIR))?
+            .set_default("sync.full_sync", true)?
+            .set_default("sync.config", false)?
+            .set_default("sync.dhcp_leases", false)?
+            .set_default("sync.gravity.group", true)?
+            .set_default("sync.gravity.adlist", true)?
+            .set_default("sync.gravity.adlist_by_group", true)?
+            .set_default("sync.gravity.domainlist", true)?
+            .set_default("sync.gravity.domainlist_by_group", true)?
+            .set_default("sync.gravity.client", true)?
+            .set_default("sync.gravity.client_by_group", true)?
             .add_source(File::with_name("ergosphere").required(false));
 
         if let Some(mut config_dir) = dirs::config_dir() {
@@ -80,6 +115,22 @@ impl AppConfig {
 
         builder = builder.add_source(Environment::with_prefix("ERGOSPHERE").separator("__"));
 
-        builder.build()?.try_deserialize()
+        let mut loaded_conf: Self = builder.build()?.try_deserialize()?;
+
+        if loaded_conf.sync.full_sync {
+            loaded_conf.sync.config = true;
+            loaded_conf.sync.dhcp_leases = true;
+            loaded_conf.sync.gravity = GravityImportOptions {
+                group: true,
+                adlist: true,
+                adlist_by_group: true,
+                domainlist: true,
+                domainlist_by_group: true,
+                client: true,
+                client_by_group: true,
+            }
+        }
+
+        Ok(loaded_conf)
     }
 }
